6b5f5156acb483e7dd312e77efeb27215ddc266a
Anish Rayaguru <rayaguruanish@gmail.com>
2025-08-19 18:40:27 +0530

    Refactor ME22B105_T1.m: Consolidate commands, enhance plotting with figures, and improve code readability

--- ME4010_anish/tut1/ME22B105_T1.m
+++ ME4010_anish/tut1/ME22B105_T1.m
@@ -1,26 +1,20 @@
 %ME22B105_T1
 %Anish Rayaguru
 %ME4010 control systems
-
-
-clc;
-clear;
-close all;
+clc;clear;close all;
 
 %defining the transfer function
 g1 = tf(1, [1 2 7 10 7 1]);
 
-
 %to create rootlocus plot - exercise 1 part 2
+figure(1);
 rlocus(g1);
 
-
-
 [r,k] = rlocus(g1);
-
 %plotting the poles of g1- exercise 2 part 1
 poles = r(:,1);
-plot(poles, 'o');
+figure(2);
+plot(poles, 'o', 'MarkerFaceColor', 'white', 'MarkerSize', 10);
 grid on;
 
 %identifying the stability of the system - exercise 2 part 2
@@ -44,15 +38,11 @@ else
 end
 
 %step response of the system - exercise 2 part 3
-step(g1);
+figure(3);
+step(g1); 
 %from step plot you can conclude that the system is stable
 
 g2 = tf([1 -3], [1 2 7 10 7 1]); %to add a zero at 3
 
-rlocus(g2);
-
-
-
-
-
-
+figure(4);
+rlocus(g2); 
