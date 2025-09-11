a093c5caf101856637adb9daaddcc574ff6f7a2f
anish rayaguru <rayaguruanish@gmail.com>
2025-09-11 06:42:36 +0530

    Refactor class4_state_space.m to include controllability check and update system matrices

--- ME4010_anish/classes/class4_state_space.m
+++ ME4010_anish/classes/class4_state_space.m
@@ -1,9 +1,14 @@
 clc; clear; close all;
 
-a = [-2 0; 0 -5];
-b = [1; 3];
+a = [-1 1 0; 0 -1 0; 0 0 2];
+b = [0; 4; 3];
 
 
 %ctrb - controllability matrix
 z = ctrb(a,b);
 
+if rank(z) == size(a,1)
+    disp('System is controllable');
+else
+    disp('System is not controllable');
+end
