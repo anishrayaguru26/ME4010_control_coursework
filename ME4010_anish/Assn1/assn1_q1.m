c9eb2efa114efc312771dd8a3a3199e3aa861a9b
anish rayaguru <rayaguruanish@gmail.com>
2025-09-18 10:20:40 +0530

    Implement code changes to enhance functionality and improve performance

--- ME4010_anish/Assn1/assn1_q1.m
+++ ME4010_anish/Assn1/assn1_q1.m
@@ -1,7 +1,12 @@
 G = zpk([-5], [-2, -4, -7, -9],[1]);
 
-rlocus(G);
+rlocus(G); hold on
 zeta = 0.7;
-ang = 180 - arctand(zeta);
 
-r = 100;
+ang = 180 - acosd(zeta);
+
+r = 40;
+
+plot([0, r*cosd(ang)], [0, r*sind(ang)], '--'); hold off
+
+
